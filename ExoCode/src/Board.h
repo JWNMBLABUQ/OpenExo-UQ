faf693f8bf97dd918473ba79d5dc95fa73cce036
JWNMBLABUQ <j.williamson@uq.edu.au>
2025-10-28 09:29:56 +1000

    Revert "Initial Update for the UQ "HeelYeah" device."
    This reverts commit e91362498f83cab1853b07fe0ac7303fbb7d7ec5.

--- ExoCode/src/Board.h
+++ ExoCode/src/Board.h
@@ -189,8 +189,8 @@
                 
                 const unsigned int speed_check_pin = 33;
 
-                const unsigned int left_ankle_angle_pin;
-                const unsigned int right_ankle_angle_pin;
+                const unsigned int left_ankle_angle_pin = A16;
+                const unsigned int right_ankle_angle_pin = A17;
             #endif
             
             const unsigned int motor_enable_on_state = HIGH;
@@ -520,8 +520,8 @@
 				
 				const unsigned int speed_check_pin;
 				
-				const unsigned int left_ankle_angle_pin = A13;
-				const unsigned int right_ankle_angle_pin = A12;
+				const unsigned int left_ankle_angle_pin;
+				const unsigned int right_ankle_angle_pin;
 				
 				// I2C 
 				// SDA 18
